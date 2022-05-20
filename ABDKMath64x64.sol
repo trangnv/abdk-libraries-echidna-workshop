56f2bf1451b7b9a6690d93ce3ca8e8d73e650200
ggrieco-tob <gustavo.grieco@trailofbits.com>
2022-05-20 18:54:25 +0200

    removed replaced assert by require in ABDKMath64x64.sol

--- ABDKMath64x64.sol
+++ ABDKMath64x64.sol
@@ -707,7 +707,7 @@ library ABDKMath64x64 {
         if (xl < lo) xh -= 1;
         xl -= lo; // We rely on overflow behavior here
 
-        assert (xh == hi >> 128);
+        require (xh == hi >> 128);
 
         result += xl / y;
       }
